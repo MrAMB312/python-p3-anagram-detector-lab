f6a6ee09126ed336a735881f859478956432f765
Alex Blomarz <a.blomarz@gmail.com>
2025-06-20 16:55:22 -0500

    complete python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def word(self):
+        return self._word
+
+    @word.setter
+    def word(self, word):
+        if isinstance(word, str):
+            self._word = word
+        else:
+            print("word must be a string")
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in candidates if sorted(candidate) == sorted_word]
